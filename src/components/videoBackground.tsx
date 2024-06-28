b18a113c807c935a33035d2c22f3a3ecbb2c52a4
Your Name <ssandroo35@gmail.com>
2024-06-29 01:04:30 +0400

    Qa done

--- src/components/videoBackground.tsx
+++ src/components/videoBackground.tsx
@@ -1,8 +1,13 @@
 const BackgroundVideo = () => {
     return (
-        <div className="fixed top-0 left-0 w-full h-full overflow-hidden z-[-1]">
-            <video className="absolute top-1/2 left-1/2 w-full h-full object-cover transform -translate-x-1/2 -translate-y-1/2" autoPlay loop muted>
-                <source src="/11969278_3840_2160_30fps.mp4" type="video/mp4" />
+        <div className="fixed top-0 left-0 w-full h-screen overflow-hidden z-0">
+            <video
+                className="absolute top-1/2 left-1/2 w-auto min-w-full min-h-full max-w-none transform -translate-x-1/2 -translate-y-1/2"
+                autoPlay
+                loop
+                muted
+            >
+                <source src="/11969278_3840_2160_30fps.mp4" type="video/mp4"/>
                 Your browser does not support the video tag.
             </video>
         </div>
