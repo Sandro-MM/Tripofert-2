d88843a23d810c5cc7cd3d7cdc16b059e87fa169
Your Name <ssandroo35@gmail.com>
2024-07-04 15:38:05 +0400

    main page done

--- src/components/videoBackground.tsx
+++ src/components/videoBackground.tsx
@@ -6,8 +6,9 @@ const BackgroundVideo = () => {
                 autoPlay
                 loop
                 muted
+
             >
-                <source src="/11969278_3840_2160_30fps.mp4" type="video/mp4"/>
+                <source src="/tripofert-short.mp4" type="video/mp4"/>
                 Your browser does not support the video tag.
             </video>
         </div>
