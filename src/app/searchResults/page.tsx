0c3ebe650a92e540b46351a6f92eb8a20cb79b49
Your Name <ssandroo35@gmail.com>
2024-08-01 21:43:28 +0400

    use client

--- src/app/searchResults/page.tsx
+++ src/app/searchResults/page.tsx
@@ -12,15 +12,26 @@ const MapComponent = dynamic(() => import('@/components/map/mapComponent'), {
     ssr: false,
 });
 
-export default function Page({params}) {
-    const searchParams = useSearchParams()
-    const departureId = searchParams.get('departureId')
-    const [departure, setDeparture] = useState<searchPlaceInterface>({"id": +searchParams.get('departureId'), "name": searchParams.get('departureName'), "country": searchParams.get('departureCountry'), "latitude": +searchParams.get('departureLatitude'), "longitude": +searchParams.get('departureLongitude')});
-    const [destination, setDestination] = useState<searchPlaceInterface>({"id": +searchParams.get('destinationId'), "name": searchParams.get('destinationName'), "country": searchParams.get('destinationCountry'), "latitude": +searchParams.get('destinationLatitude'), "longitude": +searchParams.get('destinationLongitude')});
+export default function Page({ params }) {
+    const searchParams = useSearchParams();
+    const [departure, setDeparture] = useState<searchPlaceInterface>({
+        id: +searchParams.get('departureId') || 0,
+        name: searchParams.get('departureName') || '',
+        country: searchParams.get('departureCountry') || '',
+        latitude: +searchParams.get('departureLatitude') || 0,
+        longitude: +searchParams.get('departureLongitude') || 0
+    });
+    const [destination, setDestination] = useState<searchPlaceInterface>({
+        id: +searchParams.get('destinationId') || 0,
+        name: searchParams.get('destinationName') || '',
+        country: searchParams.get('destinationCountry') || '',
+        latitude: +searchParams.get('destinationLatitude') || 0,
+        longitude: +searchParams.get('destinationLongitude') || 0
+    });
     const [date, setDate] = useState<Date | undefined>(undefined);
     const [passengers, setPassengers] = useState<number | string | undefined>(searchParams.get('passengers'));
-    const [mapDeparture, setMapDeparture] = useState<searchPlaceInterface>(departure)
-    const [mapDestination, setMapDestination] = useState<searchPlaceInterface>(destination)
+    const [mapDeparture, setMapDeparture] = useState<searchPlaceInterface>(departure);
+    const [mapDestination, setMapDestination] = useState<searchPlaceInterface>(destination);
     const [duration, setDuration] = useState<any>(undefined);
     const [distance, setDistance] = useState<any>(undefined);
     const [points, setPoints] = useState<any>(undefined);
@@ -28,7 +39,6 @@ export default function Page({params}) {
     const parseDateFromQuery = (dateString: string): Date | undefined => {
         if (!dateString) return undefined;
         const decodedDate = decodeURIComponent(dateString);
-
         const dateObject = new Date(decodedDate);
         if (!isNaN(dateObject.getTime())) {
             return dateObject;
@@ -37,29 +47,43 @@ export default function Page({params}) {
             return undefined;
         }
     };
+
     useEffect(() => {
         if (searchParams.get('date')) {
             const parsedDate = parseDateFromQuery(searchParams.get('date'));
             setDate(parsedDate);
         }
-    }, [searchParams.get('date')]);
+    }, [searchParams]);
 
-    const id = params.data
+    const id = params.data;
+
     return (
         <main className="relative w-full min-h-screen">
-            <ThemeSwitch/>
-            <Suspense fallback={<div>Loading search results...</div>}>
-
-            <SearchFilter setMapDeparture={setMapDeparture} setMapDestination={setMapDestination} departure={departure}
-                          destination={destination} setDeparture={setDeparture} setDestination={setDestination}
-                          date={date} setDate={setDate} passengers={passengers} setPassengers={setPassengers} distance={distance} points={points}/>
-
-            <RouteBarComponent points={points} departure={mapDeparture.name} destination={mapDestination.name}/>
+            <ThemeSwitch />
+            <SearchFilter
+                setMapDeparture={setMapDeparture}
+                setMapDestination={setMapDestination}
+                departure={departure}
+                destination={destination}
+                setDeparture={setDeparture}
+                setDestination={setDestination}
+                date={date}
+                setDate={setDate}
+                passengers={passengers}
+                setPassengers={setPassengers}
+                distance={distance}
+                points={points}
+            />
+            <RouteBarComponent points={points} departure={mapDeparture.name} destination={mapDestination.name} />
             <MapProvider>
-                <MapComponent departure={mapDeparture} setPoints={setPoints} setDistance={setDistance}
-                              setDuration={setDuration} destination={mapDestination}/>
+                <MapComponent
+                    departure={mapDeparture}
+                    setPoints={setPoints}
+                    setDistance={setDistance}
+                    setDuration={setDuration}
+                    destination={mapDestination}
+                />
             </MapProvider>
-            </Suspense>
         </main>
     );
 }
