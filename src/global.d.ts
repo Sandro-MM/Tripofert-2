751020d85d7de47a2f428591f077482bf4de80af
Your Name <sandro.martiashvili@yahoo.com>
2025-01-03 22:25:23 +0400

    Revert "messenger chat try 1"
    This reverts commit bfec97daa81d3884383d0148764de60a5e5b1164.

--- src/global.d.ts
+++
@@ -1,9 +0,0 @@
-interface Window {
-    FB: {
-        init: (options: { xfbml: boolean; version: string }) => void;
-        XFBML: {
-            parse: () => void;
-        };
-    };
-    fbAsyncInit: () => void;
-}
